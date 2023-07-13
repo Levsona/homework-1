3f75391e023ee825a14f60e38ca3a9bcfc555d43
Levsona <futkaradzelevo00@gmail.com>
2023-07-13 18:51:45 +0400

    Final

---
+++ homework.js
@@ -0,0 +1,8 @@
+console.log(4+6);
+console.log(3+11);
+console.log(3*5);
+console.log('hello levan');
+console.log("hello levan")
+console.log(`hello levan`)
+console.log('A' + ''+'Z')
+console.log("hello" + "" + "levan")
